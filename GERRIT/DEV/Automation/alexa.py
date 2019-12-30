a18180fe05057ecdf2c5cf77a3ff2d69ab63e1c8
PraneethAlaghari <praneethalaghari@gmail.com>
2019-12-30 15:10:54 +0530

    1577698852.1013913

--- GERRIT/DEV/Automation/alexa.py
+++ GERRIT/DEV/Automation/alexa.py
@@ -6,6 +6,7 @@ import time
 import sys
 import os
 import subprocess
+import welcome_message
 
 try:
 	import winshell
@@ -20,9 +21,12 @@ chunk_size	= 2048
 microphone_list = sr.Microphone.list_microphone_names()
 microphone_to_select = microphone_list[1]
 
+#Voice messages configuration
+
 repeat_strings = ['Speak now!!','Are you There??','I am waiting for your command..','On your Command master!!','Are you Slept?']
-max_attemps_exceeded_strings = ["Seems you are busy!! Catch you later! Bye!","Dont waste my time Master!! Bye!","Hmm.. Its enough i am going to sleep"]
+max_attemps_exceeded_strings = ["Seems you are busy!! Catch you later! Bye!","Dont waste my time Master!! Bye!","Its enough i am going to sleep"]
 leaving_strings = ['Good bye!! See you soon','Ok !! bye !! Catch you later','Bye!! Have a nice day ahead','Hope i served you better today','Always at your help!! Master']
+not_understood_strings = ["Sorry i cant understand what you said!!! May be you can ask!!! what can i do"]
 
 def speak_now(source):
 	try :
@@ -31,9 +35,8 @@ def speak_now(source):
 		return audio_data
 	except:
 		engine.say(random.choice(repeat_strings))
-		engine.runAndWait()
 		return 0
-
+			
 def interact(source):
 	num_of_attempts = 0
 	while True:
@@ -45,8 +48,29 @@ def interact(source):
 		elif num_of_attempts > 2:
 			engine.say(random.choice(max_attemps_exceeded_strings))
 			sys.exit(0)
+			
+def change_voice():
+	engine = pyttsx3.init()
+	engine.setProperty('voice','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0')			
+			
+			
 
-					
+#Decorator function implemented on engine.say() function to modify its behavior.
+#i.e To print what bot is saying
+# To prevent adding engine.runAndWait() everytime we execute say() function
+		
+def dec_engine_say(engine_say_func):
+	def addon(*args,**kwargs):
+		print("Alexa :" + args[0])
+		engine_say_func(*args,**kwargs)
+		pyttsx3.init().runAndWait()
+	return addon
+	
+pyttsx3.init().say = dec_engine_say(pyttsx3.init().say)
+	
+	
+#Functions
+			
 def openexplorer():
 	os.system('explorer')
 	return
@@ -57,37 +81,44 @@ def emptyrecyclebin():
 	return 
 		
 def run(r):
-	engine.say("How may i help you.. :")
-	engine.runAndWait()
-
+	
+	engine.say("How may i help you.. !!")
+	
 	while True:
 		with sr.Microphone(sample_rate = sample_rate, chunk_size = chunk_size) as source :
 				r.adjust_for_ambient_noise(source)
 				command = interact(source)
 				
-				if command == 'open explorer':
+				if command in ['open explorer','windows explorer','explorer']:
 					engine.say("Proceeding to open windows explorer")
-					engine.runAndWait()
 					openexplorer()
 					engine.say("Task Accomplished")
-					engine.runAndWait()
 					
-				if command == 'recycle bin':
+				elif command in ['empty recycle bin','clean recycle bin','free recycle bin']:
 					engine.say("Proceeding to empty recycle bin...")
-					engine.runAndWait()
 					emptyrecyclebin()
 					engine.say("Task Accomplished." + "All items in the bin are removed successfully")
-					engine.runAndWait()
 					
-				if command == 'bye':
+				elif command in ['weather report']:
+					engine.say("Ok.. DAVID is up with the details for you")
+					welcome_message.run()
+					change_voice()
+					
+				elif command in ['bye','good bye','get lost','see you','nothing','goodbye']:
+					engine.say(random.choice(leaving_strings))
 					break
+					
+				else:
+					engine.say(random.choice(not_understood_strings))
+					time.sleep(2)
+					
 				
 				engine.say("What else can i do for you!!")
-				engine.runAndWait()
-			
+				
 				
 if __name__ == '__main__':
 
+	global engine
 	r = sr.Recognizer()			# Starting Speech recognizer
 	engine = pyttsx3.init()		# Starting Text recognizer
 
@@ -100,5 +131,4 @@ if __name__ == '__main__':
 	
 	run(r)
 		
-	engine.runAndWait()
 	engine.stop()
--- GERRIT/DEV/Automation/welcome_message.py
+++ GERRIT/DEV/Automation/welcome_message.py
@@ -5,49 +5,63 @@ import sys
 import time
 from datetime import datetime
 
+#voices = pyttsx3.init().getProperty('voices')
+#print([voice.id for voice in voices])
 
-engine = pyttsx3.init()
-engine.setProperty('voice','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0')
+def run():
 
-# Weather 
-url = "http://api.openweathermap.org/data/2.5/weather?appid=fa2536ab2699a5077d09902716acddc2&q="
-city = 'Hyderabad'
-
-concat_url = url + city
-
-response = requests.get(concat_url)
-data = response.json()
-
-temp = data['main']['temp'] - 273.15  # Converting recieved Kelvins into C
-rounded_temp = "%.1f" % round(temp,2)
-
-
-if temp < 26:
-	climate = "cold"
-elif temp > 26 and temp < 32:
-	climate = "warm"
-else:
-	climate = "hot"
-
-current_time_hour = int(time.strftime("%H"))
-
-if current_time_hour < 12:
-	time_of_the_day = "Morning"
-elif current_time_hour > 12 and current_time_hour < 16:
-	time_of_the_day = "Afternoon"
-else:
-	time_of_the_day = "Evening"
-
-
-engine.runAndWait()
-
+	engine = pyttsx3.init()
 	
-engine.say("Hello, Good" + time_of_the_day + "Praneth")
-engine.say("Hope you are fine")
-engine.say("Its " + climate + "outside. Recording about " + rounded_temp + "Centigrade")
-engine.say("Enjoy the weather")
+	#engine.setProperty('voice','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+	
+	if __name__ == '__main__': engine.setProperty('voice','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0')
+	else: engine.setProperty('voice','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0')
+	
+	
+	
+	# Weather 
+	url = "http://api.openweathermap.org/data/2.5/weather?appid=fa2536ab2699a5077d09902716acddc2&q="
+	city = 'Hyderabad'
 
-voices = engine.getProperty('voices')
+	concat_url = url + city
 
-engine.runAndWait()
-engine.stop()
+	response = requests.get(concat_url)
+	data = response.json()
+
+	temp = data['main']['temp'] - 273.15  # Converting recieved Kelvins into C
+	rounded_temp = "%.1f" % round(temp,2)
+
+	if temp <= 26:
+		climate = "cold"
+	elif temp > 26 and temp < 32:
+		climate = "warm"
+	else:
+		climate = "hot"
+
+	current_time_hour = int(time.strftime("%H"))
+
+	if current_time_hour < 12:
+		time_of_the_day = "Morning"
+	elif current_time_hour > 12 and current_time_hour < 16:
+		time_of_the_day = "Afternoon"
+	else:
+		time_of_the_day = "Evening"
+
+
+	engine.runAndWait()
+
+		
+	engine.say("Hello, Good" + time_of_the_day + "Praneth")
+	if __name__ == '__main__': engine.say("Hope you are fine")
+	engine.say("Its " + climate + "outside. Recording about " + rounded_temp + "Centigrade")
+	engine.say("Enjoy the weather")
+
+	voices = engine.getProperty('voices')
+
+	engine.runAndWait()
+	engine.stop()
+
+
+
+if __name__ == '__main__':
+	run()
